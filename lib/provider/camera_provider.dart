ddbb42f27d1957c55085afbf3b558f09ee12b444
valendion <valendion874@gmail.com>
2023-01-15 22:51:57 +0800

    Menambahkan video

---
+++ lib/provider/camera_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:images_picker/images_picker.dart';
+import 'package:test_assignment/utils/camera_helper.dart';
+
+final pathVideoProvider =
+    StateProvider.autoDispose<String>((ref) => 'add_image');
+final isPlayingProvider = StateProvider.autoDispose<bool>((ref) => false);
